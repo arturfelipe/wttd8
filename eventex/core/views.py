afc23a83a49cb85238680e41cbf78db65387fd14
arturfelipe <arturfelipe.sousa@gmail.com>
2012-09-28 01:37:34 -0300

    Use generic views

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-from django.shortcuts import render_to_response
-from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 def homepage(request):
-    context = {'STATIC_URL': settings.STATIC_URL}
-    return render_to_response('index.html', context)
+    return direct_to_template(request, template='index.html')
