745be9c355142837d3df642f5ac1c0518da1515f
arturfelipe <arturfelipe.sousa@gmail.com>
2012-09-28 01:28:44 -0300

    Add context object to response by hand

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 def homepage(request):
-    return render_to_response('index.html')
+    context = {'STATIC_URL': settings.STATIC_URL}
+    return render_to_response('index.html', context)
