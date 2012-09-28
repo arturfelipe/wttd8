1e11800c4bf2199e3d20dd664ba6efec84dcd273
arturfelipe <arturfelipe.sousa@gmail.com>
2012-09-28 06:53:00 -0300

    Setting DEBUG = False for production enviroment purposes

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,3 +11,8 @@ urlpatterns = patterns('eventex.core.views',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG is False:   # Serve static files automatically when not on DEBUG MODE
+    urlpatterns += patterns('',
+            url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
