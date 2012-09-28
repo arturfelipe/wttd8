616758aad3a156c9de3e387136b9c02bbfdd6f96
arturfelipe <arturfelipe.sousa@gmail.com>
2012-09-28 07:26:33 -0300

    Try5

---
+++ eventex/public/js/leadingpage.js
@@ -0,0 +1,11 @@
+(function($) {
+
+    $('#ex-logo-e').fadeIn('slow', function(){
+        $('#ex-logo-x').fadeIn('slow');
+    });
+
+    $('#social-links  a').each(function(){
+        $(this).hover();
+    });
+
+})(jQuery);
