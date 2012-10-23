e371148ffc5a65449ef8947ec7b40ab0a068baf6
arturfelipe <arturfelipe.sousa@gmail.com>
2012-10-22 23:40:59 -0200

    Testing slideshare templatetag

--- eventex/core/tests/test_templatetags.py
+++ eventex/core/tests/test_templatetags.py
@@ -11,3 +11,14 @@ class YouTubeTagsTest(TestCase):
     def test_output(self):
         self.assertIn('<object', self.content)
         self.assertIn('/1', self.content)
+
+class SlideShareTest(TestCase):
+    def setUp(self):
+        context = Context({'ID': 1, 'DOC': 'Django Forms'})
+        template = Template('{% load slideshare %}{% slideshare ID DOC %}')
+        self.content = template.render(context)
+
+    def test_output(self):
+        self.assertIn('<object', self.content)
+        self.assertIn('__sse1', self.content)
+        self.assertIn('=Django Forms', self.content)
